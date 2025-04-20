67415c9a6d8bffa299754daab29383da8c6ab451
olisheikh <olisheikh707@gmail.com>
2025-04-20 18:51:39 +0600

    Babel & JSX is added

--- FirstProject/script.js
+++ FirstProject/script.js
@@ -1,4 +1,4 @@
-let leftCounter = document.querySelector(".left-counter");
+let leftCounter = document.querySelector(".left-counter");//statement
 let rightCounter = document.getElementById("right-counter");
 
 let leftImage = document.querySelector(".first-backet");
@@ -7,7 +7,7 @@ let rightImage = document.querySelector(".second-backet");
 let lefterCounterValue = parseInt(leftCounter.innerText);
 let rightCounterValue = parseInt(rightCounter.innerText);
 
-leftImage.addEventListener("click", () => {
+leftImage.addEventListener("click", () => { // () -> Parenthesis.
   lefterCounterValue--;
 
   if (lefterCounterValue >= 0) {
@@ -32,3 +32,19 @@ rightImage.addEventListener("click", () => {
     rightCounterValue = 0;
   }
 });
+
+
+"Hi, I am Faizan"
+"Anaya, how old are you?"
+"123355"
+"@#$%"
+"1234567889000000"
+
+
+let name = "Alex"; // string type
+let age = 12; // Number type Integer Number
+let price = 1000.00; // Number float Number
+let vegetarian = false; // boolean - true/false
+let mobile = null; // empty
+
+document.write(name);
